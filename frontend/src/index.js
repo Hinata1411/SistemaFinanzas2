e3547294b22cb1a438ac0b3cb2b53b0f5753e560
its_MartaBL <85373964+Hinata1411@users.noreply.github.com>
2025-08-26 04:30:44 -0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,13 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter } from 'react-router-dom'; // <-- Importa BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HelmetProvider>
-    <BrowserRouter>       {/* <-- Envuelve App en BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <App />
   </HelmetProvider>
 );
