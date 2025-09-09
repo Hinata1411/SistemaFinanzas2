bf325590d6ef4071de77e89f9c44534bc593f4f2
its_MartaBL <85373964+Hinata1411@users.noreply.github.com>
2025-09-09 14:08:35 -0600

    Cambio botones de comprobantes

--- frontend/src/components/registrar-pagos/RegistrarPagos.jsx
+++ frontend/src/components/registrar-pagos/RegistrarPagos.jsx
@@ -687,11 +687,11 @@ export default function RegistrarPagos() {
 
         <table className="rc-table rc-gastos-table rc-pagos-table">
           <colgroup>
-            <col style={{width:'200px'}}/>{/* Categoría */}
-            <col style={{width:'auto'}}/>{/* Descripción */}
-            <col style={{width:'140px'}}/>{/* Monto */}
-            <col style={{width:'200px'}}/>{/* Ref */}
-            <col style={{width:'180px'}}/>{/* Img */}
+            <col style={{width:'140px'}}/>{/* Categoría */}
+            <col style={{width:'230px'}}/>{/* Descripción */}
+            <col style={{width:'120px'}}/>{/* Monto */}
+            <col style={{width:'150px'}}/>{/* Ref */}
+            <col style={{width:'140px'}}/>{/* Img */}
             <col style={{width:'120px'}}/>{/* Acciones */}
           </colgroup>
 
